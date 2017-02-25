ada9b04bc55ddfa727261a7300b80f804b9f0b79
Dingchang Yang <i.septentrion@gmail.com>
2017-02-25 13:14:54 +0800

    Add files via upload

---
+++ Problem01/SolutionReviser.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+ * This is a better apporach to solve this problem.
+ * By applying inclusion-exclusion principle the running time
+ * can be reduced to O(1).
+ */
+
+// calculate the sum of multiples of the divisor under the given bound.
+int sum(int divisor, int bound) {
+  int fl = (bound - 1) / divisor;
+  return divisor * fl * (fl + 1) / 2;
+}
+
+int main(int argc, char *argv[]) {
+  printf("%d\n", sum(3, 1000) + sum(5, 1000) - sum(15, 1000));
+  return 0;
+}
