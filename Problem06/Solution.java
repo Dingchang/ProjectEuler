f57c4a38f333db75d844e63ca513d3acb932489b
Dingchang Yang <i.septentrion@gmail.com>
2017-05-06 17:43:40 -0400

    Add Problem 006

---
+++ Problem06/Solution.java
@@ -0,0 +1,19 @@
+
+public class Solution {
+
+  public static void main(String[] args) {
+    int num = 100;
+
+    System.out.print(sumOfFirst(num) * sumOfFirst(num) - squareSumOfFirst(num));
+  }
+  
+  // calculate sum numbers from 1 to n
+  private static int sumOfFirst(int n) {
+    return n * (n + 1) / 2;
+  }
+  
+  // calculate the sum of the square of each number from 1 to n.
+  private static int squareSumOfFirst(int n) {
+    return n * (n + 1) * (2 * n + 1) / 6;
+  }
+}
